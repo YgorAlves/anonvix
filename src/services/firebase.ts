4962d4831dd6b69c3b835d1d90dd4e40bd471de0
ygoralves <ybarbozalves@gmail.com>
2020-11-09 10:23:28 -0300

    remove firebase key

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,14 +4,7 @@ import 'firebase/auth';
 import 'firebase/analytics';
 
 const config = {
-  apiKey: "AIzaSyCn_GbtmGlqqlLaWzpII0CjB-PMAheGcj8",
-  authDomain: "anonvix.firebaseapp.com",
-  databaseURL: "https://anonvix.firebaseio.com",
-  projectId: "anonvix",
-  storageBucket: "anonvix.appspot.com",
-  messagingSenderId: "152829698285",
-  appId: "1:152829698285:web:b5ed8bd80e8e8530016ce4",
-  measurementId: "G-QG61349B3N"
+
 }
 
 firebase.initializeApp(config)
